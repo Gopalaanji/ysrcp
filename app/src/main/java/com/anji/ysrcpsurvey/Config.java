6d2dd03594afcae5b45ae3a43c36c79d9258209d
anji <gopala.anji@gmail.com>
2018-08-28 00:17:26 +0530

    login page create

---
+++ app/src/main/java/com/anji/ysrcpsurvey/Config.java
@@ -0,0 +1,10 @@
+package com.anji.ysrcpsurvey;
+
+public class Config {
+
+    public static final String MAIN = "MAIN_RTMS";
+    public static final String mobile = "MOBILE";
+    public static final String username = "USERNAME";
+    public static final String date = "DATE";
+
+}
--- app/src/main/java/com/anji/ysrcpsurvey/LoginActivity.java
+++ app/src/main/java/com/anji/ysrcpsurvey/LoginActivity.java
@@ -1,13 +1,58 @@
 package com.anji.ysrcpsurvey;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class LoginActivity extends AppCompatActivity {
+
+    EditText mobile, username;
+    SharedPreferences pref;
+    SharedPreferences.Editor editor;
+    String date;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        mobile = (EditText) findViewById(R.id.mobileno_login);
+        username = (EditText) findViewById(R.id.user_login);
+        pref = getSharedPreferences(Config.MAIN, MODE_PRIVATE);
+        String DATE_FORMAT = "yyyyMMdd";
+        SimpleDateFormat sdf;
+        sdf = new SimpleDateFormat(DATE_FORMAT);
+        Calendar c1 = Calendar.getInstance(); // today
+        date = sdf.format(c1.getTime());
+        String sharedate = pref.getString("date", "");
+        if (date.equals(sharedate)) {
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+
+    }
+
+    public void logIn(View view) {
+
+
+        String mobile_no = mobile.getText().toString().trim();
+        String user_name = username.getText().toString().trim();
+        if (mobile_no.length() == 10) {
+            editor = pref.edit();
+            editor.putString(Config.mobile, mobile_no);
+            editor.putString(Config.username, user_name);
+            editor.putString(Config.date, date);
+
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(intent);
+            finish();
+
+        } else mobile.setError("Invalied Mobile");
     }
 }
